1622394ae5eef88196bd4bfe2bad46865f70e64a
barteshwar <barteshwar@gmail.com>
2023-07-19 01:13:27 +0530

    added fonttools

--- headerfooter2.py
+++ headerfooter2.py
@@ -7,6 +7,10 @@ import os
 import subprocess
 import sys
 import gizeh
+import fontTools
+
+from fontTools.ttLib import TTFont
+font = TTFont('Oswald-Bold.ttf')
 
 """
 An alternative text clip for Moviepy, relying on Gizeh instead of ImageMagick
