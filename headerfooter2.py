e1bc26e730385831104e2cf9fb99dbbace809a93
barteshwar <barteshwar@gmail.com>
2023-07-19 01:53:48 +0530

    oswald-bold

--- headerfooter2.py
+++ headerfooter2.py
@@ -217,10 +217,10 @@ def make_video(vc,ht,ft):
 
     header_fontsize=20
     #header_fontheight=20
-    header = text_clip(header_text_split,font_family="Oswald-Bold.ttf",align='center',font_height = header_fontsize, fill_color=(255,255,255),stroke_width=0).set_duration(video_clip.duration)
+    header = text_clip(header_text_split,font_family="Oswald-Bold",align='center',font_height = header_fontsize, fill_color=(255,255,255),stroke_width=0).set_duration(video_clip.duration)
 
     while(header.size[0]<image_clip.size[0]*0.9 and header.size[1]<(image_clip.size[1]-video_clip.size[1])/2*0.8):
-        header = text_clip(header_text_split,font_family="Oswald-Bold.ttf",align='center',font_height = header_fontsize, fill_color=(255,255,255),stroke_width=0).set_duration(video_clip.duration)
+        header = text_clip(header_text_split,font_family="Oswald-Bold",align='center',font_height = header_fontsize, fill_color=(255,255,255),stroke_width=0).set_duration(video_clip.duration)
         header_fontsize=header_fontsize+1
         print(header_fontsize)
     header = text_clip(header_text_split,font_family='Oswald-Bold',font_height = header_fontsize, align='center',fill_color=(255,255,255),stroke_width=0).set_duration(video_clip.duration)
@@ -235,10 +235,10 @@ def make_video(vc,ht,ft):
     print(footer_text_split)
 
     footer_fontsize=20
-    footer = text_clip(footer_text_split,font_family="https://fonts.googleapis.com/css2?family=Roboto:wght@100&display=swap",align='center',font_height=footer_fontsize, fill_color=(255,255,255),stroke_width=0).set_duration(video_clip.duration)
+    footer = text_clip(footer_text_split,font_family="Oswald-Bold",align='center',font_height=footer_fontsize, fill_color=(255,255,255),stroke_width=0).set_duration(video_clip.duration)
 
     while(footer.size[0]<image_clip.size[0]*0.9 and footer.size[1]<(image_clip.size[1]-video_clip.size[1])/2*0.9 ):
-        footer = text_clip(footer_text_split, font_family="https://fonts.googleapis.com/css2?family=Roboto:wght@100&display=swap",align='center',font_height = footer_fontsize, fill_color=(255,255,255),stroke_width=0).set_duration(video_clip.duration)
+        footer = text_clip(footer_text_split, font_family="Oswald-Bold",align='center',font_height = footer_fontsize, fill_color=(255,255,255),stroke_width=0).set_duration(video_clip.duration)
         footer_fontsize=footer_fontsize+1
         print(footer_fontsize)
     footer = text_clip(footer_text_split, font_family='Oswald-Bold',align='center',font_height = footer_fontsize, fill_color=(255,255,255),stroke_width=0).set_duration(video_clip.duration)
