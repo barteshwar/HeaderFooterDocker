38cec6a59665185433e0341728b74899b91b421a
barteshwar <barteshwar@gmail.com>
2023-07-25 10:26:32 +0530

    changed logic for resize

--- headerfooter2.py
+++ headerfooter2.py
@@ -207,7 +207,8 @@ def make_video(vc,ht,ft,src):
     image_clip = ImageClip('blue1000.jpg').set_duration(video_clip.duration)
     print("image clip size",image_clip.size)
     if(video_clip.w>image_clip.w):
-        video_clip=video_clip.resize((image_clip.w,video_clip.h*video_clip.h/image_clip.w))
+        #video_clip=video_clip.resize((image_clip.w,video_clip.h*video_clip.h/image_clip.w))
+        video_clip=video_clip.resize(width=image_clip.w)
     else:
         image_clip=image_clip.resize((video_clip.w,video_clip.w))
     print("video clip size",video_clip.size)
