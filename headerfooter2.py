3b5154606bd586f9eb1ab8a3f16effb64ec9352d
barteshwar <barteshwar@gmail.com>
2023-07-27 16:36:53 +0530

    removed fps

--- headerfooter2.py
+++ headerfooter2.py
@@ -345,7 +345,7 @@ def make_video(vc,ht,ft,src):
     # Set the output file name and save the final clip
     output_file = "output_video.mp4"
     #final_clip.write_videofile(output_file, codec="libx264")
-    final_clip.write_videofile(output_file,fps=final_clip.fps)
+    final_clip.write_videofile(output_file)
     video_file=open("output_video.mp4",'rb')
     #video_bytes = output_file.read()
     #st.video(video_bytes) 
